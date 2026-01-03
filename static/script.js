4a2ec067fc3e2facbfe3d0a0548fc247e4cbddb7
BeForceEx3 <142899996+BeForceEx3@users.noreply.github.com>
2026-01-03 20:04:13 +0300

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    const pass = document.querySelector('input[name="pass"]').value;
-    if (pass.length < 6) {
-        e.preventDefault();
-        alert('Пароль слишком короткий');
-    }
-});
